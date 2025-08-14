c258f61007da01aa47090dd6a3ed4be66076993f
johnas-bautista <johnasbautistajr03@gmail.com>
2025-08-14 16:23:08 +0800

    commit

--- resources/views/common/verifyEmail.blade.php
+++ resources/views/common/verifyEmail.blade.php
@@ -1,6 +1,8 @@
 @extends('layout.loginpage')
 
 @section ('content')
+
+@include('layout.partials.message')
     <div style="height: 100vh; background-color:rgb(53, 56, 62);">
         @include('layout.partials.verifyOtpForm')
     </div>
